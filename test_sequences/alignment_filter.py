88f63808d006e85d0530f73c7ea9a40020c939ff
carl <icacedo@ucdavis.edu>
2024-12-17 19:31:04 -0800

    up

--- test_sequences/alignment_filter.py
+++ test_sequences/alignment_filter.py
@@ -13,22 +13,44 @@ args = parser.parse_args()
 
 def get_reads(fasta):
 
-    reads = {}
-    count = 0
     if fasta.endswith('gz'): 
         with gzip.open(fasta, 'rt') as fp:
-            for line in fp.readlines():
-                line = line.rstrip().split(' ')
-                if count == 4: count = 0
-                if count == 0:
-                    rid = line[0][1:]
-                    reads[rid] = [line]
-                else:
-                    reads[rid].append(line)
-                count += 1
+            content = fp.read().rstrip()
+            content = content.split('\n')
+    else:
+        with open(fasta, 'rt') as fp:
+            content = fp.read().rstrip()
+            content = content.split('\n')
     
+    '''
+    for line in content:
+        print(line)
+        line = line.rstrip().split(' ')
+        if count == 4: count = 0
+        if count == 0:
+            rid = line[0][1:]
+            reads[rid] = [line]
+        else:
+            reads[rid].append(line)
+            count += 1
+    '''
+    reads = {}
+    rid = ''
+    count = 0
+    for line in content:
+        if count == 0:
+            rid = line.split(' ')[0][1:]
+            reads[rid] = [line]
+            count += 1
+        else:
+            reads[rid].append(line)
+            count += 1
+        if count == 4: count = 0
+
     return reads
-            
+
+# should i write something to verify each entry has the correct info?
+# i.e. the ID is always the ID, the sequence is always the sequence...            
 r1_reads = get_reads(args.fasta_r1)
 r2_reads = get_reads(args.fasta_r2)
 
@@ -39,14 +61,13 @@ with open(args.filtered_sam, 'rt') as fp:
         line = line.rstrip().split('\t')
         read_ids.append(line[0])
 
-for i in read_ids:
-    print(i)
-    break
-
-print('#####')
-
 # now need to rewrite fasta files with only aligned reads
 # need both paired reads in seperate fasta files
 
-for rid in read_ids:
-    print(r1_reads[rid])
+
+with open('new.fastq', 'w') as file:
+    for rid in read_ids:
+        for line in r1_reads[rid]:
+            file.write(f'{line}\n')
+
+
